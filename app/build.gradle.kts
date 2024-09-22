a3f3bd216a5056a31974eacb6601450f697347ff
ean3ena <ean3ena@yandex.ru>
2024-09-22 10:06:37 +0300

    add picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,9 @@ repositories {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
+
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
