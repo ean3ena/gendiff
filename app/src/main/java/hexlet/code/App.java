349eb756f1f49d9a9f2a77b6372f576b19f0dbe6
ean3ena <ean3ena@yandex.ru>
2024-09-23 19:03:28 +0300

    refactor

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,27 +5,35 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.Callable;
+
 @Command(name = "gendiff",
         mixinStandardHelpOptions = true,
         version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable {
+public class App implements Callable {
 
-    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
-    private String format;
+    @Option(names = {"-f", "--format"},
+            paramLabel = "format",
+            description = "output format [default: stylish]")
+    private String format = "stylish";
 
-    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    @Parameters(paramLabel = "filepath1",
+            description = "path to first file")
     private String filepath1;
 
-    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    @Parameters(paramLabel = "filepath2",
+            description = "path to second file")
     private String filepath2;
 
     @Override
-    public void run() {
+    public Object call() throws Exception {
         System.out.println("Hello World!");
+        return null;
     }
 
     public static void main(String[] args) {
-        CommandLine.run(new App(), args);
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
