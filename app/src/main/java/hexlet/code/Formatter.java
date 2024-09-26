6548c5b77b8ab507bd1a832b0bc06bb120e55365
ean3ena <ean3ena@yandex.ru>
2024-09-27 00:36:56 +0300

    add new class on step 6

---
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import java.util.List;
+import java.util.Map;
+
+public class Formatter {
+
+    public static String format(List<Map<String, Object>> diff, String format) {
+
+        if (format.equals("stylish")) {
+            return formatStylish(diff);
+        }
+
+        return null;
+    }
+
+    public static String formatStylish(List<Map<String, Object>> diff) {
+
+        StringBuilder result = new StringBuilder("{\n");
+
+        for (var elem : diff) {
+            result.append("  ");
+            result.append(elem.get("status"));
+            result.append(" ");
+            result.append(elem.get("key"));
+            result.append(": ");
+            result.append(elem.get("value"));
+            result.append("\n");
+        }
+        result.append("}");
+
+        return result.toString();
+    }
+}
---
+++ app/src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class Parser {
+
+    public static Map<String, Object> parse(String filepath) throws Exception {
+
+        String fileContent = getFileContent(filepath);
+        return parseJson(fileContent);
+    }
+
+    public static String getFileContent(String filepath) throws Exception {
+
+        Path path = Paths.get(filepath).toAbsolutePath().normalize();
+        if (!Files.exists(path)) {
+            throw new Exception("Файл '" + path + "' не существует");
+        }
+
+        return Files.readString(path);
+    }
+
+    public static Map<String, Object> parseJson(String fileContent) throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(fileContent, new TypeReference<>(){});
+    }
+}
