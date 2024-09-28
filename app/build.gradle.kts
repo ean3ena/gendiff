716698ec7ba83b01942f971ba3c95eab67095d1e
ean3ena <ean3ena@yandex.ru>
2024-09-28 13:20:57 +0300

    add jackson-dataformat-yaml library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
 
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
 
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
