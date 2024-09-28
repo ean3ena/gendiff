3fc0de7efc4037502a82279c5b5eab9f904a400c
ean3ena <ean3ena@yandex.ru>
2024-09-29 00:59:06 +0300

    add new class

---
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -0,0 +1,45 @@
+package hexlet.code.formatters;
+
+import java.util.List;
+import java.util.Map;
+
+public class Plain {
+
+    public static String format(List<Map<String, Object>> diff) {
+
+        StringBuilder result = new StringBuilder();
+
+        for (var elem : diff) {
+            var status = (String) elem.get("status");
+            var key = elem.get("key");
+            var value1 = getTextByValue(elem.get("value1"));
+            var value2 = getTextByValue(elem.get("value2"));
+
+            switch (status) {
+                case "added" -> result.append("Property '").append(key)
+                        .append("' was added with value: ").append(value1).append("\n");
+                case "deleted" -> result.append("Property '").append(key)
+                        .append("' was removed").append("\n");
+                case "changed" -> result.append("Property '").append(key)
+                        .append("' was updated. From ").append(value1)
+                        .append(" to ").append(value2).append("\n");
+                default -> {}
+            }
+        }
+        return result.substring(0, result.length() - 1);
+    }
+
+    private static String getTextByValue(Object obj) {
+
+        if (obj == null) {
+            return "null";
+        }
+        if (obj instanceof String) {
+            return "'" + obj + "'";
+        }
+        if (obj instanceof Boolean || obj instanceof Number) {
+            return obj.toString();
+        }
+        return "[complex value]";
+    }
+}
---
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -0,0 +1,35 @@
+package hexlet.code.formatters;
+
+import java.util.List;
+import java.util.Map;
+
+public class Stylish {
+
+    public static String format(List<Map<String, Object>> diff) {
+
+        StringBuilder result = new StringBuilder();
+
+        result.append("{\n");
+
+        for (var elem : diff) {
+            var status = (String) elem.get("status");
+            var key = elem.get("key");
+            var value1 = elem.get("value1");
+            var value2 = elem.get("value2");
+
+            switch (status) {
+                case "added" -> result.append("  + ").append(key).append(": ").append(value1).append("\n");
+                case "deleted" -> result.append("  - ").append(key).append(": ").append(value1).append("\n");
+                case "unchanged" -> result.append("    ").append(key).append(": ").append(value1).append("\n");
+                case "changed" -> {
+                    result.append("  - ").append(key).append(": ").append(value1).append("\n");
+                    result.append("  + ").append(key).append(": ").append(value2).append("\n");
+                }
+                default -> {}
+            }
+        }
+        result.append("}");
+
+        return result.toString();
+    }
+}
